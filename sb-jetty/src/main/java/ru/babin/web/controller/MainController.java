cc9f131abbe4b69dc538f024f206210351920637
Alexander Babin <alexander.babin@gmail.com>
2017-06-08 23:45:07 +0300

    improvement

--- sb-jetty/src/main/java/ru/babin/web/controller/MainController.java
+++ sb-jetty/src/main/java/ru/babin/web/controller/MainController.java
@@ -1,15 +1,13 @@
 package ru.babin.web.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import ru.babin.web.model.Item2;
-import ru.babin.web.model.Job;
+import ru.babin.web.model.Item;
 
 
 @Controller
@@ -19,21 +17,33 @@ public class MainController {
 	public @ResponseBody String root() {
 		return "Welcome to the spring-boot application!";
 	}
-
-	@Transactional
+	
+	/*
+	 * Send data as form params (Content-Type: application/x-www-form-urlencoded)
+	 * 
+	 */
 	@PostMapping("/post")
-	public @ResponseBody String jobs(@RequestParam String q, @RequestParam String w ) {
+	public @ResponseBody String post1(@RequestParam String q, @RequestParam String w ) {
 		return String.format("jobs: q=%s , w=%s", q, w);
 	}
 	
-	@Transactional
+	/*
+     * Send data as form params (Content-Type: application/x-www-form-urlencoded)
+     * 
+     */
     @PostMapping("/post2")
-    public @ResponseBody String jobs(@RequestBody Item2 item ) {
+    public @ResponseBody String post2(Item item ) {
         return String.format("post2: %s", item);
     }
+	
+	
+	/*
+	 * Send data as JSON-object (Content-Type: application/json)
+	 */
+    @PostMapping("/post3")
+    public @ResponseBody String post3(@RequestBody Item item ) {
+        return String.format("post3: %s", item);
+    }
 
-	@GetMapping("/get")
-	public @ResponseBody Job statistics() {
-		return new Job();
-	}
+	
 }
--- sb-jetty/src/main/java/ru/babin/web/model/Item2.java
+++
@@ -1,8 +0,0 @@
-package ru.babin.web.model;
-
-import lombok.Data;
-
-public @Data class Item2{
-    private String q;
-    private String w;
-}
